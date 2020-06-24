56014be392a58d82f345c1df5bef140153c05773
VHM <taolaolataom@gmail.com>
2020-06-24 16:29:22 +0700

    Updates

---
+++ precache-manifest.606cfcc41e13ea403b35170cc71cc7ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ca342f99dae035823bb3fbe14fc9c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74da599b9fae602bc7ad",
+    "url": "/static/css/main.e66027c0.chunk.css"
+  },
+  {
+    "revision": "b2f3ce25c3f174eb37fb",
+    "url": "/static/js/2.94816119.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.94816119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74da599b9fae602bc7ad",
+    "url": "/static/js/main.75bf0c52.chunk.js"
+  },
+  {
+    "revision": "156490742f317cda665f",
+    "url": "/static/js/runtime-main.6a1cdc3f.js"
+  }
+]);
